c380e6be44bcbd79a1ef56eebd67c080803ee5da
a2c6201 <a2c6201@gmail.com>
2023-01-17 17:57:38 +0900

    style

--- src/thread/New/screens/index.js
+++ src/thread/New/screens/index.js
@@ -1,10 +1,8 @@
-import { Typography } from "@mui/material";
 import ThreadNew from "../components/ThreadNew";
 
 const New = () => {
   return (
     <>
-      <Typography>スレッド作成</Typography>
       <ThreadNew />
     </>
   );
