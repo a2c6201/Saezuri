48d8ba576bce265a83bec5b6383550ed70027026
a2c6201 <a2c6201@gmail.com>
2023-01-23 08:32:26 +0900

    改善点コメント

--- src/thread/Post/screens/index.js
+++ src/thread/Post/screens/index.js
@@ -7,10 +7,13 @@ import PostList from "../components/PostList";
 
 const Post = () => {
   const { id } = useParams();
-  const { data: data, error } = useSWR(
+  const { data, error } = useSWR(
     `https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads/${id}/posts`,
     fetcher
   );
+  // ここで、POSTしたデータを整形しても
+  // 1。GETし直す (useSWRじゃないほうがいいかも)　＜--　これ楽
+  // 2。Post Formからデータを渡してデータを整形　　<-- エラー起きやすいがフェッチする回数が減ってパフォーマンス面○
 
   if (error) return <Typography>faild to load</Typography>;
   if (!error && !data) return <Typography>Loading</Typography>;
@@ -29,7 +32,7 @@ const Post = () => {
 //   {
 //     "id": "string",
 //     "post": "string"
-//   }
+//   },
 // ]
 // }
 
